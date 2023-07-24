733e688723900c7061d1e18f7df9a2cf0ddf1318
dane-meister <dane.meister@stonybrook.edu>
2023-07-24 17:57:46 -0400

    initial push

---
+++ Client/pinempire_backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '6gohxs6b',
+    dataset: 'production'
+  }
+})
---
+++ Client/pinempire_frontend/src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
+import Login from './components/Login';
+import Home from './container/Home';
+import { fetchUser } from './utils/fetchUser';
+
+const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+    if(!user) navigate('/login');
+  }, [])
+  
+  return (
+    <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_AUTH_ID}`}>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </GoogleOAuthProvider>
+  )
+}
+
+export default App
